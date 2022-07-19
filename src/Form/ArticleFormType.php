8867a45f9dd6288cca83273415e2dc3c428129f0
jO mAY <104192198+Jomay23@users.noreply.github.com>
2022-07-19 21:53:30 +0200

    jojo3

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,13 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
@@ -30,7 +32,12 @@ class ArticleFormType extends AbstractType
                 'data_class' => null,
                 'attr' => [
                     'data-default-file' => $options['photo']
-                ]
+                ],
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Valider',
